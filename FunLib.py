7e2a84c8ff77c3d16627cbbff040f7396afd7c83
Schoolbadge <maarten.demuynck@gmail.com>
2023-01-27 10:02:35 +0100

    funlib pick random + add default dir structure

--- FunLib.py
+++ FunLib.py
@@ -1,17 +1,19 @@
 import random
 import os
+import glob
 
 
 def success(rpi, deviceConfig):
-    movieFilePath = getRandomFile(deviceConfig["mediaDirPath"])
-    rpi.playMovie(movieFilePath)
+    successLib = os.path.join(deviceConfig['mediaDirPath'], 'success/**/*')
+    print(successLib)
+    mediaFilePath = getRandomFile(successLib)    
+    rpi.playMovie(mediaFilePath)
 
 
 def fail(rpi):
-    rpi.playMovie("media/nee.mp4")
+    rpi.playMovie("media/fail/nee.mp4")
 
 
 def getRandomFile(mediaDirPath):
-    movieFilePath = random.choice([x for x in os.listdir(
-        mediaDirPath) if os.path.isfile(os.path.join(mediaDirPath, x))])
+    movieFilePath = random.choice(glob.glob(mediaDirPath, recursive=True))
     return movieFilePath
